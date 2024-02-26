cc45427563f8a9fa6305681acc0991860dec9247
saeki7036 <114328780+saeki7036@users.noreply.github.com>
2024-02-27 00:41:23 +0900

    Add/EnemySpawn

---
+++ src/Assets/Karioki/SpawnerEnemyController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerEnemyController : MonoBehaviour
+{
+    [SerializeField] private GameObject Enemy;
+    [SerializeField] private float SpawnIntarval = 5f;
+    [SerializeField] private float DestroyIntarval = 30f;
+    [SerializeField] private int SpawnMenberNum = 3;
+    [SerializeField] private float SpawnRange = 1.5f;
+    private float SpawnTime = 0;
+    private float DestroyTime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnTime += Time.deltaTime;
+        DestroyTime += Time.deltaTime;
+
+        if (SpawnTime > SpawnIntarval)
+        {
+            SpawnTime = 0f;
+            for(int i = 0; i < SpawnMenberNum; i++)
+            {
+                float randomValue_X = Random.Range(-SpawnRange, SpawnRange);
+                float randomValue_Z = Random.Range(-SpawnRange, SpawnRange);
+                Vector3 SpawnPos = transform.position + new Vector3(randomValue_X, 0f, randomValue_Z);
+                GameObject.Instantiate(Enemy, SpawnPos, Quaternion.Euler(0f, 0f, 0f));
+            }
+          
+        }
+
+        if (DestroyTime > DestroyIntarval)
+        {
+            DestroyTime = 0f;
+            Destroy(this.gameObject);
+        }
+
+    }
+}
