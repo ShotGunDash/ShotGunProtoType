f4fc80e5a6a1eb886819d40db5313447eba226d6
mocha_show <fuka.sho0224@gmail.com>
2024-03-14 22:14:47 +0900

    ShieldEnemyControllerの名前かぶりによるバグの修正

--- src/Assets/FukanoTest/ShieldEnemyHitController.cs
+++ src/Assets/FukanoTest/ShieldEnemyHitController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class ShieldEnemyController : MonoBehaviour, EnemyInterface
+public class ShieldEnemyHitController : MonoBehaviour, EnemyInterface
 {
     private Rigidbody rb;
     [SerializeField] private int HP;
